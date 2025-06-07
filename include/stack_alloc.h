84b07467fefe5f27ba2529d035fdff2d579acd6d
Gokul <146622216+0smic@users.noreply.github.com>
2025-06-08 03:07:34 +0530

    Update stack_alloc.h

--- include/stack_alloc.h
+++ include/stack_alloc.h
@@ -1,1 +1,10 @@
+#ifndef CUSTOM_MALLOC_H
+#define CUSTOM_MALLOC_H
+
+void *heap_alloc(size_t size);
+void heap_free(void *ptr);
+void *heap_realloc(void *ptr, size_t size);
+void heap_colle(void);
+
+#endif
 
